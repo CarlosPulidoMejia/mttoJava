a952b2623e28926e5c963ee09c2b6dcd7534c532
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-02-07 11:22:24 -0600

    inicio post

--- src/main/java/com/bim/reporte/mantenimiento/controller/TipoDependenciaController.java
+++ src/main/java/com/bim/reporte/mantenimiento/controller/TipoDependenciaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,11 @@ public class TipoDependenciaController {
 	@PutMapping("/detalleDependencia/{id}")
 	public void guardarDependencia (@PathVariable int id, @RequestBody DetalleDependenciaRequest detalleReq) {
 		dependenciaService.modificarDependencia(id, detalleReq);
+	}
+	
+	@PostMapping("guardarDependencia")
+	public void guardarDependencia (@RequestBody DetalleDependenciaRequest dependencia) {
+		
 	}
 	
 }
